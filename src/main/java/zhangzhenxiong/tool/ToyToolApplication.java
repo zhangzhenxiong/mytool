b4f73b8fae4ceb60105a5cab64711e731f8fb90b
zhangzhenxiong <zzxzhangzhenxiong@gmail.com>
2019-01-26 17:19:48 +0900

    add my tool project zhangzhenxiong

---
+++ src/main/java/zhangzhenxiong/tool/ToyToolApplication.java
@@ -0,0 +1,14 @@
+package zhangzhenxiong.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyToolApplication.class, args);
+	}
+
+}
+
