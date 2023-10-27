77456fe25790004af118b4308a14ed2b999a7cd1
alex <zhenxiong@gmail.com>
2023-10-27 11:38:19 +0000

    aaaa

--- src/main/java/zhangzhenxiong/tool/controller/HomeController.java
+++ src/main/java/zhangzhenxiong/tool/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    //abcdefg
+    //abcdefgh
 	@GetMapping("/")
 	public String home() {
 		return "home";
